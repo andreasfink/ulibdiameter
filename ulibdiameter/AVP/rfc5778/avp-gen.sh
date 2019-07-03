20d1a6aa091d6dbf22f924fb54ebc75bf69284c2
Andreas Fink <afink@amdtelecom.net>
2019-07-03 11:30:03 +0300

    fix up dups

--- ulibdiameter/AVP/rfc5778/avp-gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-diameter-avp-gen \
-	--definitions avp-table.txt \
-	--group-definitions ./avp-group-defs/ \
-	--write-avp-headers \
-	--write-avp-methods \
-	--overwrite \
-	--directory . --verbose
